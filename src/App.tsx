fabc02f4f6d12c3eadacb498f8ff5fbda6b2a57d
liana <wang.lianyan@aishu.cn>
2023-11-30 18:16:24 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as eva from '@eva-design/eva';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {ApplicationProvider} from '@ui-kitten/components';
+import * as React from 'react';
+import HomeScreen from './pages/Home';
+import SettingsScreen from './pages/Settings';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <ApplicationProvider {...eva} theme={eva.dark}>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </ApplicationProvider>
+    </NavigationContainer>
+  );
+}
